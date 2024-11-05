3d4baeeeea135f46557f142873f74a1cccdc277c
Greg Haskins <greg@manetu.com>
2024-11-05 18:18:00 -0500

    Add support for token-type
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- src/manetu/tokenizer_loadtest/actions/tokenize/core.clj
+++ src/manetu/tokenizer_loadtest/actions/tokenize/core.clj
@@ -1,14 +1,14 @@
 ;; Copyright © Manetu, Inc.  All rights reserved
 
 (ns manetu.tokenizer-loadtest.actions.tokenize.core
-  (:require  [clojure.java.io :as io]
-             [clojure.core.async :as async]
-             [taoensso.timbre :as log]
-             [promesa.core :as p]
-             [crypto.random]
-             [manetu.tokenizer-loadtest.core :as core]
-             [manetu.tokenizer-loadtest.utils :refer [async-map]]
-             [manetu.tokenizer-loadtest.driver.api :as driver.api]))
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :as async]
+            [taoensso.timbre :as log]
+            [promesa.core :as p]
+            [crypto.random]
+            [manetu.tokenizer-loadtest.core :as core]
+            [manetu.tokenizer-loadtest.utils :refer [async-map]]
+            [manetu.tokenizer-loadtest.driver.api :as driver.api]))
 
 (defn file->lines
   "opens the file at 'path' and reads in all lines to a collection"
@@ -23,14 +23,15 @@
   {:mrn mrn :values (repeatedly tokens-per-job #(crypto.random/bytes value-min))})
 
 (defn job-exec
-  [{:keys [driver] :as ctx} {:keys [mrn values] :as record}]
-  (-> (driver.api/tokenize driver mrn values)
+  [{:keys [driver token-type] :as ctx} {:keys [mrn values] :as record}]
+  (-> (driver.api/tokenize driver {:mrn mrn :type token-type} values)
       (p/then (fn [tokens]
                 (assoc record :tokens tokens)))))
 
 (defn exec [{:keys [jobs] :as ctx} vault-list]
   (log/debug "using vault-list:" vault-list)
   (let [vaults (file->lines vault-list)]
+    (log/debug "ctx:" ctx)
     (log/debug "found vaults:" vaults)
     @(->> (async/to-chan! (take jobs (cycle vaults)))
           (async-map (partial generate-values ctx))
